6147fdd1138606b8004485bcf561337c7497598c
Luke Larsen <Luke@lukehanslarsen.com>
2020-05-25 22:56:17 -0400

    Networking updates

--- scripts/network.js
+++ scripts/network.js
@@ -4,11 +4,23 @@ if(networkEnabled){
     var request = new XMLHttpRequest()
     var url = 'https://' + explorer
     // Open a new connection, using the GET request on the URL endpoint
-    request.open('GET', url + '/api/v1/' + publicKeyForNetwork, true)
+    request.open('GET', url + '/api/v1/address/' + publicKeyForNetwork, true)
     request.onload = function() {
       var data = JSON.parse(this.response)
+      document.getElementById("balance").innerHTML = data['balance'];
+      document.getElementById("totalReceived").innerHTML = data['totalReceived'];
+      document.getElementById("totalSent").innerHTML = data['totalSent'];
+      var typeNumber = 4;
+      var errorCorrectionLevel = 'L';
+      var qr = qrcode(typeNumber, errorCorrectionLevel);
+      qr.addData('dogecash:'+ data['addrStr']);
+      qr.make();
+      document.getElementById("addrStrQR").innerHTML = qr.createImgTag();
+      document.getElementById("addrStr").innerHTML = data['addrStr'];
+
       document.getElementById("NetworkingJson").innerHTML = this.response;
       console.log(data)
+      console.log()
     }
     // Send request
     request.send()
